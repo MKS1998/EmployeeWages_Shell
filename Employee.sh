0e7b45fa67f1c434f15b258e8fcb276feca43b21
MKS1998 <nksayyad2900@outlook.com>
2021-08-13 22:25:14 +0530

    Employee wages using Switch Case

--- Employee.sh
+++ Employee.sh
@@ -1,31 +1,23 @@
 #!/bin/bash -x
 
-<<<<<<< HEAD
-randomCheck=$((RANDOM%2))
-=======
 isFullTime=1
 isPartTime=2
 empRatePerHour=20
 
 randomCheck=$((RANDOM%3))
->>>>>>> UC3_PartTimeEmpWages
 
-if [ $randomCheck -eq 1 ]
-then
-	empHrs=8;
-	salary=$(($empHrs*$empRatePerHour));
-<<<<<<< HEAD
-        echo "Employee Daily Wages : " $salary
-else
-	echo "Employee is absent"
-=======
-	echo "Employee FullTime wages : " $salary
-elif [ $randomCheck -eq $isPartTime ]
-then
-        empHrs=4;
-	salary=$(($empHrs*$empRatePerHour));
-	echo "Employee PartTime wages : " $salary
-else
-	echo "Employee is absent"	
->>>>>>> UC3_PartTimeEmpWages
-fi
+case $randomCheck in
+	$isFullTime)
+		empHrs=8;
+		salary=$(($empHrs*$empRatePerHour));
+		echo "Employee FullTime wages : " $salary
+		;;
+	$isPartTime)
+        	empHrs=4;
+		salary=$(($empHrs*$empRatePerHour));
+		echo "Employee PartTime wages : " $salary
+		;;
+	*)
+		echo "Employee is absent"	
+		;;
+esac
