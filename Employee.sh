7497c9a47bda7cd7f8aacbeb34d916c541dbf776
MKS1998 <nksayyad2900@outlook.com>
2021-08-13 22:12:28 +0530

    Employee PartTime wages.

--- Employee.sh
+++ Employee.sh
@@ -1,13 +1,21 @@
 #!/bin/bash -x
 
 isFullTime=1
-randomCheck=$((RANDOM%2))
+isPartTime=2
+empRatePerHour=20
+
+randomCheck=$((RANDOM%3))
 
 if [ $randomCheck -eq $isFullTime ]
 then
-	empRatePerHour=20;
 	empHrs=8;
 	salary=$(($empHrs*$empRatePerHour));
+	echo "Employee FullTime wages : " $salary
+elif [ $randomCheck -eq $isPartTime ]
+then
+        empHrs=4;
+	salary=$(($empHrs*$empRatePerHour));
+	echo "Employee PartTime wages : " $salary
 else
-	salary=0;
+	echo "Employee is absent"	
 fi
