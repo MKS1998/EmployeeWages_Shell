001e816b1395536a620a38a64a25400b9b5ae306
MKS1998 <nksayyad2900@outlook.com>
2021-08-14 19:12:49 +0530

    Calculate wages till condition.

--- Employee.sh
+++ Employee.sh
@@ -1,10 +1,32 @@
-#!/bin/bash -x
+#! user/bin/bash     -x
 
-empRatePerHour=20
-totalWorkingdays=20
-empHrs=8;
+IS_PRESENT_FULL_TIME=1
+IS_PRESENT_HALF_TIME=2
+EMP_RATE_PER_HR=20
+MAX_WORKING_DAYS=20
+MAX_WORK_HR=100
+totalWorkingHr=0
+day=1
+totalsalary=0
+while [ $day -le $MAX_WORKING_DAYS ] && [ $totalWorkingHr -lt $MAX_WORK_HR ]
+do
+        ((day++))
+	empCheck=$((RANDOM%3))
 
-	totalHours=$empHrs*$totalWorkingdays;
-	salary=$(($totalHours*$empRatePerHour));
-	echo "Employee Monthly wages : " $salary
+        case  $empCheck  in
+                $IS_PRESENT_FULL_TIME)
+                     empHr=8 
+			;;
+                $IS_PRESENT_HALF_TIME)
+                      empHr=4 
+			;;
+                *)
+                       empHr=0 
+			;;
+        esac
+        totalWorkingHr=$(( $totalWorkingHr + $empHr ))
+	echo "totalWorkingHr: "$totalWorkingHr
+done
 
+totalSalary=$(( $totalWorkingHr * $EMP_RATE_PER_HR ))
+echo "total Wages: " $totalSalary
